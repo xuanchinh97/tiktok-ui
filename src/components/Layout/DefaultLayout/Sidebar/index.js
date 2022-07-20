298bfa4f8e485b92af3bca7deaa7e24a3862fa66
chinhdz97 <kqtchcheng97@gmail.com>
2022-07-20 15:31:40 +0700

    layout default

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import classNames from 'classnames/bind'
+import styles from './Sidebar.module.scss'
+
+const cx = classNames.bind(styles)
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <aside className={cx('wrapper')}>Sidebar</aside>
   )
 }
 
